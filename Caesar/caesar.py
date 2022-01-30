0d4175d67655febe89d4243da6a7bfc67771d941
whoamins <40am1ns@protonmail.com>
2022-01-30 18:25:30 +0300

    Caesar

---
+++ Caesar/caesar.py
@@ -0,0 +1,20 @@
+from sys import argv
+
+
+def encrypt(text: str, key: int) -> str:
+    result = str()
+
+    for char in text:
+        if char.isupper():
+            result += chr((ord(char) + key - ord('A')) % 26 + ord('A'))
+        else:
+            result += chr((ord(char) + key - ord('a')) % 26 + ord('a'))
+    
+    return result
+
+
+if __name__ == "__main__":
+    text = argv[1]
+    key = int(argv[2])
+
+    print(encrypt(text, key))
