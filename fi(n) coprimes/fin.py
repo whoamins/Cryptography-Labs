fbaa761905b3977ec24386cf75e4f40814c07779
whoamins <ilsaf.nabiullin@whoamins.ru>
2022-04-04 22:18:20 +0300

    fi(n)

--- fi(n) coprimes/fin.py
+++ fi(n) coprimes/fin.py
@@ -1,11 +1,15 @@
 from Coprime import coprime
 
 
-def print_all_coprime(n: int) -> None:
+def print_all_coprime(n: int) -> int:
+    count = 0
     for i in range(n):
         if coprime.coprime(n, i):
             print(i)
+            count += 1
+
+    return count
 
 
 if __name__ == "__main__":
-    print_all_coprime(30)
+    print("fi(n)=", print_all_coprime(30))
